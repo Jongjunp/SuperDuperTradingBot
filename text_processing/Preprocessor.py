d19210f30ed7ec1b06d3800f9f2393a63cf5a32c
Jongjun Park <pjjkey@kaist.ac.kr>
2020-02-04 00:20:20 +0900

    selenium.common.exceptions.ElementClickInterceptedException: Message: element click intercepted 크롤링 하다 중간에 멈춤/ 데이터베이스 저장 확인 / cnn 가동 시작

--- text_processing/Preprocessor.py
+++ text_processing/Preprocessor.py
@@ -1,4 +1,5 @@
 import sqlite3
+import pandas as pd
 
 class Preprocessor:
     def __init__(self,DBloc,TableName):
@@ -8,4 +9,14 @@ class Preprocessor:
 
     def dbOpen(self):
         cur = self.connection.cursor()
-        cur.execute()
+        dataList = cur.execute("SELECT * From "+ self.TableName)
+        cols = [column[0]for column in dataList.description]
+        data_result = pd.DataFrame.from_records(data=dataList.fetchall(),columns=cols)
+
+        return data_result
+
+    def CutWords(self):
+        datalist = self.dbOpen()
+
+
+
